6ef4bd22572edeb008ccb7edb3d67a76a8e0d5f5
Marco Rojas <Marco1095@outlook.com>
2015-06-22 16:51:37 -0500

    Basic querys for api requests

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express 	= require('express');
 
 
 //DB Connection config
-require('./config/db.js')
+require('./config/db.js');
 
 //Middleware
 app.use(morgan('dev'));
@@ -15,15 +15,35 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 // Routes
-app.get('/status', function (req, res){
-  res.sendStatus(200);
-});
-
 app.get('/', function (req, res){
-  res.status(200);
   res.render('index',{seccion : 'Inicio'});
 });
 
+app.get('/almacen', function (req, res){
+  var sql = 'SELECT * FROM `Almacen`';
+  connection.query(sql, function(err, results) {
+    if(err) {
+    	console.log('Query Error: ' + err);
+    }
+    console.log(results);
+  	res.render('almacen',{seccion : 'Almacenes', results : results});
+  });
+});
+
+app.get('/pieza', function (req, res){
+  var sql = 'SELECT * FROM `Pieza`';
+  connection.query(sql, function(err, results) {
+    if(err) {
+    	console.log('Query Error: ' + err);
+    }
+  console.log(results);
+  res.render('pieza',{seccion : 'Piezas', results : results});
+  });
+});
+
+app.get('/', function (req, res){
+  res.render('index',{seccion : 'Existencias'});
+});
 
 
 app.listen(port, function () {

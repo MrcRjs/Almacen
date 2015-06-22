f27ac1289b3d8fc4d265d9aebb91bf1f79e5608c
Marco Rojas <Marco1095@outlook.com>
2015-06-22 08:43:39 -0500

    Morgan dev and fs

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-var express = require('express');
-var app = express();
+var express 	= require('express');
+	app 		= express();
+    fs          = require('fs')
+	morgan 		= require('morgan')
+
 
 //DB Connection config
 require('./config/db.js')
 
-app.get('/', function(req, res){
-  res.send('hello world');
+//Middleware
+app.use(morgan('dev'))
+
+// Routes
+app.get('/status', function (req, res, next)
+{
+  res.sendStatus(200);
 });
 
-app.listen(8080);
+app.get('/', function (req, res, next)
+{
+  var data = fs.readFileSync(__dirname + '/views/index.html');
+  res.status(200);
+  res.header('Content-Type', 'text/html');
+  res.end(data.toString());
+});
+
+
+
+app.listen(8080, function () {
+  console.log( "Almacen listening 8080 ")
+});

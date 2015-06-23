bb590ecb4e4fd0e400a84b0932aecf02f3403496
Marco Rojas <Marco1095@outlook.com>
2015-06-23 08:02:22 -0500

    Set environment and error handler

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express 	= require('express');
-	app 		= express();
-	morgan 		= require('morgan');
+var express 	 = require('express');
+	app 		 = express();
+	morgan 		 = require('morgan');
+	errorHandler = require('errorhandler');
 
 
 //DB Connection config
 require('./config/db.js');
+
+//Server conf
+app.set('env', 'development')
+if('production' == app.get('env')) {
+  app.set('port', 80);
+}
+else{app.set('port', 8080);}
+
 
 //Middleware
 app.use(morgan('dev'));
@@ -12,10 +21,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
 // Routes
 require('./routes/apiRoutes')(app);
+require('./routes/wwwRoutes')(app);
 
-var port = 8080;
-app.listen(port, function () {
-  console.log( "Almacen listening at " + port)
+app.listen(app.get('port'), function () {
+  console.log( "Almacen listening at " + app.get('port'));
 });

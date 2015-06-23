fa16684a3cce887dde20f2adb00e120b82393a8e
Marco Rojas <Marco1095@outlook.com>
2015-06-23 08:04:37 -0500

    Get Pieza route query api/www

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,20 +6,18 @@ module.exports = (function(app) {
   /* Main                         */
   /********************************/
 
-  app.get('/', function (req, res){
-    res.render('index',{seccion : 'Inicio'});
-  });
-
   /********************************/
   /* Almacenes                    */
   /********************************/
-  app.get('/almacen', function (req, res){
-    var sql = 'SELECT * FROM `Almacen`';
+  app.post('/api/:almacen/:estanteria?', function (req, res){
+    var alm = req.params.almacen;
+        //est = req.params.estanteria ? req.params.estanteria : NULL; 
+        sql = 'SELECT * FROM `Almacen`';
     connection.query(sql, function(err, results) {
       if(err) {
       	console.log('Query Error: ' + err);
       }
-    	res.render('almacen',{seccion : 'Almacenes', results : results});
+
     });
   });
 
@@ -27,14 +25,24 @@ module.exports = (function(app) {
   /* Piezas                       */
   /********************************/
 
-  app.get('/pieza', function (req, res){
-    var sql = 'SELECT * FROM `Pieza`';
-    connection.query(sql, function(err, results) {
+  app.get('/api/pieza/:tipo/:modelo?', function (req, res){
+    var tipo   = req.params.tipo
+        modelo = req.params.modelo
+        query  = {
+          sql       : 'SELECT * FROM Existencias USE INDEX (fkPieza) WHERE PiezaT=?',
+          timeout   : 200,
+          values    : [tipo]
+        };
+    if(modelo){
+      query.values.push(modelo); 
+      query.sql += 'AND PiezaM=?';
+    }
+    connection.query(query, function(err, results) {
       if(err) {
-      	console.log('Query Error: ' + err);
+        response.status(500).send('Sorry, we\'ve got an internal error.');
+        console.log('Query Error: ' + err);
       }
-    console.log(results);
-    res.render('pieza',{seccion : 'Piezas', results : results});
+      res.send(results)
     });
   });
 });
---
+++ routes/wwwRoutes.js
@@ -0,0 +1,38 @@
+module.exports = (function(app) {
+  //restful module
+  var rest = require('restler');
+
+  /********************************/
+  /* Main                         */
+  /********************************/
+
+  app.get('/', function (req, res){
+    res.render('index',{seccion : 'Inicio'});
+  });
+
+  /********************************/
+  /* Almacenes                    */
+  /********************************/
+  app.get('/almacen', function (req, res){
+    var sql = 'SELECT * FROM `Almacen`';
+    connection.query(sql, function(err, results) {
+      if(err) {
+      	console.log('Query Error: ' + err);
+      }
+    	res.render('almacen',{seccion : 'Almacenes', results : results});
+    });
+  });
+
+  /********************************/
+  /* Piezas                       */
+  /********************************/
+  app.get('/pieza/:tipo/:modelo?', function (req,res) {
+    var tipo    = req.params.tipo;
+        modelo  = req.params.modelo;
+        route = 'http://localhost:' + app.get('port') + '/api/pieza/' + tipo;
+        if(modelo){ route += '/' + modelo };
+        rest.get(route).on('complete', function (piezas) {
+          res.render('pieza',{seccion : 'Piezas', results : piezas});
+        });
+    });
+});
